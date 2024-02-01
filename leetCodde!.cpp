763d8074a9d5bffbd5656cda837425895ba5ac06
aadi2525 <aadirajhodlurkar@gmail.com>
2024-02-01 17:54:21 +0530

    1-feb

--- leetCodde!.cpp
+++ leetCodde!.cpp
@@ -35,3 +35,68 @@ public:
 
     }
 };
+
+// 80. Remove Duplicates from Sorted Array II
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    int k = 1;
+    int ocurace =1;
+
+    for(int i = 1;i<nums.size();i++){
+        if(nums[i] == nums[i-1]){
+            ocurace++;
+        }else{
+            ocurace =1;
+        }
+
+        if(ocurace<=2){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+        return k;
+    
+    }
+    
+};
+
+// 189. Rotate Array
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // vector<int> temp(nums.size());
+        // for(int i=0;i<nums.size();i++){
+        //     temp[(i+k)%nums.size()] = nums[i];
+        // }
+        //  nums = temp ;
+
+        
+         k = k%nums.size();
+        int l= 0,r=nums.size()-1; 
+
+        while(l<r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
+        }
+        l=0,r = k-1;
+         while(l<r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
+        }
+        l=k,r =nums.size()-1;
+         while(l<r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
+        }
+
+
+    }
+};
+
+
